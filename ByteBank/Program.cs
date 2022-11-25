42dd4f3cdc08fbf95244401dba34318f35a913b9
fpontes006 <fpontes006@gmail.com>
2022-11-25 00:43:18 -0300

    [feat] TryCath

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,7 +10,47 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Exceção Tratada.");
+            }
             Console.ReadLine();
         }
+
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Exceção Tratada.");
+
+            }
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            Dividir(10, divisor);
+        }
+
+        public static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+
+
     }
 }
